33a2f967f16e12ff518aac049d6f0977c9486212
bryanm260996 <josephcevallos2000@gmail.com>
2023-10-12 14:06:11 -0400

    Created background

--- game.py
+++ game.py
@@ -1,5 +1,39 @@
-print('Hello World!')
-list1=[x for x in range(0,10)]
-print(list1)
-for numbers in list1:
-    print(numbers)
+import pygame
+import sys
+
+#Initialize Pygame:
+pygame.init()
+
+#screen dimensions:
+
+screen_width=800
+screen_height=600
+
+#define colors
+BLUE=(0,0,255)
+BROWN=(224,161,52)
+
+screen= pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Blue background with brown rectangle')
+
+#MAIN LOOP
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+    #fill screen with blue
+    screen.fill(BLUE)
+
+    #draw the bottom rectangle
+    rectangle_height=200
+    pygame.draw.rect(screen, BROWN, (0,screen_height-rectangle_height, screen_width, rectangle_height))
+
+    #update display
+
+    pygame.display.flip()
+
+pygame.quit()
+
+
